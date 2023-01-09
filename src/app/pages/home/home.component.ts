4c2da446e1669b4bc15bb54439c3b0ca7f77fa11
iisaiaso <isaiasibarraonofre@gmail.com>
2023-01-09 16:58:16 -0500

    estilos finales

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,6 +10,30 @@ export class HomeComponent {
   valor!: string
 
   title2 = 'currency pipes'
-  moneda!:string
-  option!:string
+  moneda!: string
+  option!: string
+
+  estudiante = [
+    {
+      "Nombre": "Diego",
+      "Apellido": "Arica",
+      "Casado": false,
+      "Direccion": "Calle 35 # 43 28",
+      "Telefono": 3859720
+    },
+    {
+      "Nombre": "Kevin",
+      "Apellido": "Bueno",
+      "Casado": false,
+      "Direccion": "CR 16A # 53 28",
+      "Telefono": 31485579954
+    },
+    {
+      "Nombre": "Natalia",
+      "Apellido": "Palomino",
+      "Casado": false,
+      "Direccion": "CR 25 # 52 33",
+      "Telefono": 32255945555
+    }
+  ]
 }
